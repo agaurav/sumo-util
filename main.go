b5664f3c2b245d34e3ff1ee7e322571217843783
Ashish Gaurav <agaurav@sumologic.com>
2022-06-02 13:20:08 +0530

    remove comments

--- main.go
+++ main.go
@@ -1,31 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"github.com/agaurav/sumo-util/cmd"
 )
 
 func main() {
 
-	fmt.Println("a")
 	cmd.Execute()
-	//t, err := libs.DashboardTemplate()
-	//
-	//if err != nil {
-	//	panic(err)
-	//}
-	//
-	//dashJSONBytes, err := ioutil.ReadFile("samples/dash.json")
-	//
-	//dashObj := libs.Dashboard{}
-	//
-	//json.Unmarshal(dashJSONBytes, &dashObj)
-	//
-	//pretty.Println(dashObj)
-	//
-	//err = t.Execute(os.Stdout, dashObj)
-	//
-	//if err != nil {
-	//	panic(err)
-	//}
 }
